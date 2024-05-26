4be7a05e86e0a93049eab7ea29f25889a607ddbd
manoj pandey <manoj64314@gmail.com>
2024-05-26 09:55:56 +0530

    change1 change2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+<<<<<<< HEAD
         System.out.println(1);
+=======
+        System.out.println(2);
+>>>>>>> 8d3292d ( change1)
     }
 
     public String getMessage() {
